c6837f55a75743dfa8767ee1a5c71e689baac6d8
Rahul Mishra <rahul.mishra@medable.com>
2022-12-27 18:02:20 +0530

    Invoke edge function everytime the url changes

--- background.js
+++ background.js
@@ -5,5 +5,27 @@ browser.webNavigation.onCommitted.addListener((evt) => {
     return;
   }
   console.error(evt.url)
+  postData('https://pijjwctkypdkcbvsaupt.functions.supabase.co/Diskussed-backend', { url: evt.url })
+  .then((data) => {
+    console.error(data);
+  });
 });
+
+
+
+async function postData(url = '', data = {}) {
+  const response = await fetch(url, {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpamp3Y3RreXBka2NidnNhdXB0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzIxMzczMjQsImV4cCI6MTk4NzcxMzMyNH0.6l6-TIsVaHquK-4hG4_o-tVmteYGP_uLz4j3a-NQLC4'
+    },
+    referrerPolicy: 'no-referrer'
+    body: JSON.stringify(data)
+  });
+  return response.json();
+}
+
 
