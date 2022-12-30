8d6847da37a2b1d82cbbf210bc9e1bcd748140b4
Rahul Mishra <rahul.mishra@medable.com>
2022-12-30 11:41:47 +0530

    Minor refactor: move constants to top

--- background.js
+++ background.js
@@ -5,6 +5,12 @@ const BLACKLIST_URLS = [
   "https://www.google.com/search",
 ];
 
+const BACKEND_URL =
+  "https://pijjwctkypdkcbvsaupt.functions.supabase.co/Diskussed-backend";
+// Row level Security is enabled, so safe to expose
+const ANON_AUTH_TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpamp3Y3RreXBka2NidnNhdXB0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzIxMzczMjQsImV4cCI6MTk4NzcxMzMyNH0.6l6-TIsVaHquK-4hG4_o-tVmteYGP_uLz4j3a-NQLC4";
+
 // https://stackoverflow.com/a/11156533/3363206
 browser.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (!changeInfo.url) {
@@ -36,10 +42,7 @@ async function fetchFromServiceIfNotCached(url) {
   let cacheEntry = await browser.storage.local.get(url);
   console.error("cache", cacheEntry);
   if (!Object.keys(cacheEntry).length) {
-    const data = await postData(
-      "https://pijjwctkypdkcbvsaupt.functions.supabase.co/Diskussed-backend",
-      { hashedUrl: url }
-    );
+    const data = await fetchDiscussions({ hashedUrl: url });
     console.error("data fetched", data);
     cacheEntry = { [url]: JSON.stringify(data) };
     console.error("cacheEntry", cacheEntry);
@@ -48,15 +51,14 @@ async function fetchFromServiceIfNotCached(url) {
   setBadge(JSON.parse(cacheEntry[url]).length);
 }
 
-async function postData(url = "", data = {}) {
-  const response = await fetch(url, {
+async function fetchDiscussions(data = {}) {
+  const response = await fetch(BACKEND_URL, {
     method: "POST",
     mode: "cors",
     cache: "no-cache",
     headers: {
       "Content-Type": "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpamp3Y3RreXBka2NidnNhdXB0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzIxMzczMjQsImV4cCI6MTk4NzcxMzMyNH0.6l6-TIsVaHquK-4hG4_o-tVmteYGP_uLz4j3a-NQLC4",
+      Authorization: `Bearer ${ANON_AUTH_TOKEN}`,
     },
     referrerPolicy: "no-referrer",
     body: JSON.stringify(data),
